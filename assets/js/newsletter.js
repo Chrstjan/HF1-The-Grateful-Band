f0f36b62d408b2a155eb47cda08f9894fe2bd518
Chrstjan <ps5christian@hotmail.com>
2024-05-28 12:59:50 +0200

    Started styling the newsletter signup

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -8,15 +8,28 @@ export const newsletter = () => {
     let newsletter = `
         <form>
             <fieldset>
+                <button class="close-modal-btn">X</button>
                 <header>
                     <h2>Sign up to recive news about our next concert</h2>
                 </header>
+                <label for="name">Your Name</label>
+                <input class="form-name" name="name" type="text" placeholder="John Doe" />
+                <label for="email">Your Email</label>
+                <input class="form-email" name="email" type="email" placeholder="none@fake.com" />
+                <span class="form-btn-container">
+                    <input type="submit" value="Sign Up" placeholder="Sign Up" />
+                    <input type="reset" value="Reset" placeholder="Reset" />
+                </span>
             </fieldset>
         </form>`;
 
         newsletterContainer.innerHTML += newsletter;
         app.appendChild(newsletterContainer);
-        console.log("lol");
+
+        const closeBtn = document.querySelector(".close-modal-btn");
+        closeBtn.addEventListener("click", () => {
+            newsletterContainer.remove();
+        })
 }
 
 const clearApp = () => {
