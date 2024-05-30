439c6b762f282431fef98f9d9dffa108fc3c08e6
Chrstjan <ps5christian@hotmail.com>
2024-05-30 13:00:47 +0200

    Moved smaller js files into the other folder for more file structure

--- assets/js/Components/app.js
+++ assets/js/Components/app.js
@@ -4,9 +4,12 @@ import {
 } from "../Components/band/band.js";
 import { buildConcertView } from "./band/concert.js";
 import { buildHero } from "./gallery/heroGallery.js";
-import { showNewsletter, shownav } from "./other.js";
-import { scrollChecker } from "./upArrow.js";
+import { showNewsletter, shownav } from "./other/other.js";
+import { scrollChecker } from "./other/upArrow.js";
 
+/**
+ * - This function calls all the functions from different files that should run on page load
+ */
 export const initPage = () => {
   buildHero();
   shownav();
